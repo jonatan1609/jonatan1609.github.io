7203af6cafe79241693125bb23c9585ae4870cc8
Jonatan <5377074j@gmail.com>
2020-07-16 16:06:13 +0300

    added a big file for example and html tags' order changed

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 function show(doc) {
     let req = new XMLHttpRequest();
-    req.open('GET', `/blogs/${doc}`);
+    req.open('GET', `/jonatan1609.github.io/blogs/${doc}`);
     req.onreadystatechange = () => {
         let div = document.getElementById('content');
         div.innerHTML = req.responseText;
