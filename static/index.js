542c69e990d07f1012541256f3baa83dfed66884
Jonatan <5377074j@gmail.com>
2020-07-16 14:41:10 +0300

    add blog

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 function show(doc) {
     let req = new XMLHttpRequest();
-    req.open('GET', `/static${doc}`);
+    req.open('GET', `/blogs/${doc}`);
     req.onreadystatechange = () => {
         let div = document.getElementById('content');
         div.innerHTML = req.responseText;
