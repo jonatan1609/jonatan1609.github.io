b9460b37b072ee4cecd3e9955a5b395f7d2ea20d
Jonathan <5377074j@gmail.com>
2021-09-26 01:48:57 +0300

    Remake

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function show(doc) {
-    let req = new XMLHttpRequest();
-    req.open('GET', `/jonatan1609.github.io/blogs/${doc}`);
-    req.onreadystatechange = () => {
-        let div = document.getElementById('content');
-        div.innerHTML = req.responseText;
-    }
-    req.send();
-}
